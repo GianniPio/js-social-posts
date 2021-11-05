7b94440c1d2495cd39f4dfb5c2101009474a1f53
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-05 15:49:55 +0100

    logical stemps

---
+++ main.js
@@ -0,0 +1,19 @@
+// - Creiamo il nostro array di oggetti che rappresentano ciascun post.
+// Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+// nome autore,
+// foto profilo,
+// data,
+// testo del post,
+// immagine (non tutti i post devono avere una immagine) [quindi gestisco il caso],
+// numero di likes.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo i post del nostro feed.
+// - Rendiamo il tasto “Mi Piace” cliccabile con incremento del counter dei likes.
+
+
+// 1. Creare nell'array di oggetti
+
+// 2. Stampare i relativi elementi nell'HTML (non tutte con le immagini)
+    // 2.1 Se la chiave immagini è vuota, non stampa l'immagine ->if
+
+// 3. Rendere il tasto like cliccabile e aumentare il numero di like 
+
