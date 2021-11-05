f2b83108083fd9456b200f0e62757d33f190a241
GianniPio <giannipiopellegrino00@gmail.com>
2021-11-05 18:20:52 +0100

    add fuction and correction img

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ let contenitore = document.querySelector(".posts-list");
 
 // Creazione dell'array di oggetti
 
-const post = 
+const posts = 
 
 [
 
@@ -30,7 +30,7 @@ const post =
         'nomeAutore' :"Phil Mangione",
         'dataPost' : "1 mese fa",
         'testoPost' : "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-        'immaginePost' : "https://unsplash.it/300/300?image=",
+        'immaginePost' : "https://unsplash.it/568/304?image=",
         'numberLikes' : 80
     },
 
@@ -46,7 +46,7 @@ const post =
         'nomeAutore' : "Jon Snow",
         'dataPost' : "31/12/2020",
         'testoPost' : "Non c'è vergogna nell'avere paura, mi diceva sempre mio padre. Quello che conta è come l'affrontiamo.",
-        'immaginePost' : "https://unsplash.it/300/300?image=",
+        'immaginePost' : "https://unsplash.it/568/304?image=",
         'numberLikes' : 2
     },
 
@@ -54,7 +54,7 @@ const post =
         'nomeAutore' : "Simone Icardi",
         'dataPost' : "12/04/2021",
         'testoPost' : "Come va?",
-        'immaginePost' : "https://unsplash.it/300/300?image=",
+        'immaginePost' : "https://unsplash.it/568/304?image=",
         'numberLikes' : 1
     },
 
@@ -70,54 +70,39 @@ const post =
         'nomeAutore' : "Giulio Cesare",
         'dataPost' : "01/20/21",
         'testoPost' : "Tu quoque, Brute, fili mi?",
-        'immaginePost' : "https://unsplash.it/300/300?image=",
+        'immaginePost' : "https://unsplash.it/568/304?image=",
         'numberLikes' : 80
     }
 
 ]
 
-console.log(post);
+console.log(posts);
 
-for (let i = 0; i < post.length; i++) {
+for (let i = 0; i < posts.length; i++) {
     
-    const {nomeAutore, dataPost, testoPost, immaginePost, numberLikes} = post[i];
+    const {nomeAutore, dataPost, testoPost, immaginePost, numberLikes} = posts[i];
 
     if (immaginePost[i] == "") {
         
-        contenitore.innerHTML +=
-        `
-        <div class="post">
-            <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
-                    </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${nomeAutore}</div>
-                        <div class="post-meta__time">${dataPost}</div>
-                    </div>                    
-                </div>
-            </div>
-            <div class="post__text">${testoPost}</div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
-                    </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${numberLikes}</b> persone
-                    </div>
-                </div> 
-            </div>            
-        </div>
+     stampaPostImmagine(nomeAutore, dataPost, testoPost, i, numberLikes);
         
-        `
     } else {
 
-        contenitore.innerHTML +=
+     stampaWithoutIMG(nomeAutore, dataPost, testoPost, immaginePost, i, numberLikes);
+
+    }
+    
+}
+
+
+
+
+
+
+ 
+function stampaPostImmagine(elem1, elem2, elem3, number ,elem4) {
+
+    contenitore.innerHTML +=
         `
         <div class="post">
             <div class="post__header">
@@ -126,34 +111,65 @@ for (let i = 0; i < post.length; i++) {
                         <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
                     </div>
                     <div class="post-meta__data">
-                        <div class="post-meta__author">${nomeAutore}</div>
-                        <div class="post-meta__time">${dataPost}</div>
+                        <div class="post-meta__author">${elem1}</div>
+                        <div class="post-meta__time">${elem2}</div>
                     </div>                    
                 </div>
             </div>
-            <div class="post__text">${testoPost}</div>
-            <div class="post-image">
-                <img src="${immaginePost}" alt>
-            </div>
+            <div class="post__text">${elem3}</div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" data-postid=${number}>
                             <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                             <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${numberLikes}</b> persone
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${elem4}</b> persone
                     </div>
                 </div> 
             </div>            
         </div>
         
         `
+}
 
-    }
+function stampaWithoutIMG(elem1, elem2, elem3, elem4, number, elem5) {
 
+    contenitore.innerHTML +=
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${elem1}</div>
+                    <div class="post-meta__time">${elem2}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${elem3}</div>
+        <div class="post-image">
+            <img src="${elem4}" alt>
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" data-postid==${number}>
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${elem5}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
     
+    `
 }
 
